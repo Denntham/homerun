0504fb6d740f0cdbe611c4f14a6a5a7d10dabd8d
Denntham <thamdennis340@gmail.com>
2021-07-02 13:59:24 +0700

    first commit

---
+++ constants/Theme.js
@@ -0,0 +1,46 @@
+export default {
+  COLORS: {
+    DEFAULT: '#DCDCDC',
+    PRIMARY: '#9C26B0',
+    LABEL: '#FE2472',
+    INFO: '#00BCD4',
+    ERROR: '#F44336',
+    SUCCESS: '#4CAF50',
+    WARNING: '#FF9800',
+    MUTED: '#979797',
+    INPUT: '#DCDCDC',
+    ACTIVE: '#9C26B0',
+    BUTTON_COLOR: '#9C26B0',
+    PLACEHOLDER: '#9FA5AA',
+    SWITCH_ON: '#9C26B0',
+    SWITCH_OFF: '#D4D9DD',
+    GRADIENT_START: '#6B24AA',
+    GRADIENT_END: '#AC2688',
+    PRICE_COLOR: '#EAD5FB',
+    BORDER_COLOR: '#E7E7E7',
+    BLOCK: '#E7E7E7',
+    ICON: '#4A4A4A',
+  },
+  SIZES: {
+    BLOCK_SHADOW_RADIUS: 2,
+  },
+  COLOR: {
+    WHITE: '#FFFFFF',
+    GREY : '#1F1F1F',
+    DARKRED: '#730220',
+    PINK : '#F20775',
+    VIOLET: '#B35FD9',
+    DARKBLUE : '#0367A6',
+    SKYBLUE: '#04D9D9',
+  }
+};
+
+
+  // static const Color white = Color(0xFFffffff);
+  // static const Color lightgrey = Color(0xFFDCDCDC);
+  // static const Color grey = Color(0xFF1f1f1f);
+  // static const Color darkRed = Color(0xFF730220);
+  // static const Color pink = Color(0xFFF20775);
+  // static const Color violet = Color(0xFFbe5fd9);
+  // static const Color darkBlue = Color(0xFF0367A6);
+  // static const Color skyBlue = Color(0xFF04d9d9);
